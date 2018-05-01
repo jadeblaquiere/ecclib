ffc87d6d6be58b12a5c8e68424a9fe0a05566cac
Joseph deBlaquiere <jadeblaquiere@yahoo.com>
2018-05-01 14:53:25 +0000

    point class in golang

---
+++ ecgo/point.go
@@ -0,0 +1,265 @@
+//BSD 3-Clause License
+//
+//Copyright (c) 2018, jadeblaquiere
+//All rights reserved.
+//
+//Redistribution and use in source and binary forms, with or without
+//modification, are permitted provided that the following conditions are met:
+//
+//* Redistributions of source code must retain the above copyright notice, this
+//  list of conditions and the following disclaimer.
+//
+//* Redistributions in binary form must reproduce the above copyright notice,
+//  this list of conditions and the following disclaimer in the documentation
+//  and/or other materials provided with the distribution.
+//
+//* Neither the name of the copyright holder nor the names of its
+//  contributors may be used to endorse or promote products derived from
+//  this software without specific prior written permission.
+//
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+//FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+//OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+//OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package ecgo
+
+// #cgo LDFLAGS: -lecc -lgmp
+// #include <ecurve.h>
+// #include <ecpoint.h>
+// #include <field.h>
+// #include <gmp.h>
+// #include <stdlib.h>
+//
+// typedef _mpECP_t *mpECP_ptr;
+//
+// mpECP_ptr malloc_ECPoint() {
+//     return (mpECP_ptr)malloc(sizeof(_mpECP_t));
+// }
+//
+// __mpz_struct *_pgx_of_cv(mpECurve_t cv) {
+//     return cv->G[0];
+// }
+//
+// __mpz_struct *_pgy_of_cv(mpECurve_t cv) {
+//     return cv->G[1];
+// }
+//
+// int _mpECP_supports_base_mul(mpECP_t pt) {
+//     return pt->base_bits != 0;
+// }
+//
+// char *mpECP_alloc_out_str(mpECP_t pt, int compress) {
+//     int leng;
+//     char *buf;
+//
+//     leng = mpECP_out_strlen(pt, compress);
+//     buf = (char *)malloc((leng + 1)*sizeof(char));
+//     assert(buf != NULL);
+//     mpECP_out_str(buf, pt, compress);
+//     return buf;
+// }
+//
+// void free_ECPoint(mpECP_ptr pt) {
+//     free(pt);
+// }
+//
+import "C"
+
+import (
+	//"bytes"
+	//"encoding/base64"
+	//"encoding/hex"
+	//"encoding/binary"
+	//"errors"
+	//"fmt"
+	//"io/ioutil"
+	"math/big"
+	//"os"
+	//"reflect"
+	"runtime"
+	//"strconv"
+	//"strings"
+	//"time"
+	"unsafe"
+)
+
+// Max size of Fp values is based on _MPFP_MAX_LIMBS * sizeof(mp_limb_t)
+// const _MPFP_Max_Bytes = (32 * 8)
+
+type Point struct {
+	ecp *C._mpECP_t
+	cv  *Curve
+}
+
+func NewPointXY(x, y *big.Int, c *Curve) (z *Point) {
+	var xmpz, ympz C.__mpz_struct
+
+	C.mpz_init(&xmpz)
+	C.mpz_init(&ympz)
+
+	bigint_to_c_mpz_signed(&xmpz, x)
+	bigint_to_c_mpz_signed(&ympz, y)
+
+	z = new(Point)
+	z.ecp = C.malloc_ECPoint()
+	z.cv = c
+	C.mpECP_init(z.ecp, c.ec)
+	C.mpECP_set_mpz(z.ecp, &xmpz, &ympz, c.ec)
+
+	C.mpz_clear(&xmpz)
+	C.mpz_clear(&ympz)
+	runtime.SetFinalizer(z, point_clear)
+	return z
+}
+
+func NewPointNeutral(c *Curve) (z *Point) {
+
+	z = new(Point)
+	z.ecp = C.malloc_ECPoint()
+	z.cv = c
+	C.mpECP_init(z.ecp, c.ec)
+	C.mpECP_set_neutral(z.ecp, c.ec)
+
+	runtime.SetFinalizer(z, point_clear)
+	return z
+}
+
+func NewPointGenerator(c *Curve) (z *Point) {
+
+	z = new(Point)
+	z.ecp = C.malloc_ECPoint()
+	z.cv = c
+	C.mpECP_init(z.ecp, c.ec)
+	C.mpECP_set_mpz(z.ecp, C._pgx_of_cv(c.ec), C._pgy_of_cv(c.ec), c.ec)
+
+	runtime.SetFinalizer(z, point_clear)
+	return z
+}
+
+func NewPointURandom(c *Curve) (z *Point) {
+
+	z = new(Point)
+	z.ecp = C.malloc_ECPoint()
+	z.cv = c
+	C.mpECP_init(z.ecp, c.ec)
+	C.mpECP_urandom(z.ecp, c.ec)
+
+	runtime.SetFinalizer(z, point_clear)
+	return z
+}
+
+func NewPointFromString(gst string, c *Curve) (z *Point) {
+
+	z = new(Point)
+	z.ecp = C.malloc_ECPoint()
+	z.cv = c
+	C.mpECP_init(z.ecp, c.ec)
+	cstr := C.CString(gst)
+	C.mpECP_set_str(z.ecp, cstr, c.ec)
+	C.free(unsafe.Pointer(cstr))
+
+	runtime.SetFinalizer(z, point_clear)
+	return z
+}
+
+func point_clear(z *Point) {
+	C.mpECP_clear(z.ecp)
+	C.free_ECPoint(z.ecp)
+}
+
+func (z *Point) Affine() (x, y *big.Int) {
+	var xmpz, ympz C.__mpz_struct
+
+	C.mpz_init(&xmpz)
+	C.mpz_init(&ympz)
+
+	C.mpz_set_mpECP_affine_x(&xmpz, z.ecp)
+	C.mpz_set_mpECP_affine_y(&ympz, z.ecp)
+
+	x = c_mpz_to_bigint(&xmpz)
+	y = c_mpz_to_bigint(&ympz)
+
+	C.mpz_clear(&xmpz)
+	C.mpz_clear(&ympz)
+
+	return x, y
+}
+
+func (z *Point) IsNeutral() bool {
+	pt := z.ecp
+	isn := pt.is_neutral
+	if int(isn) == 0 {
+		return false
+	}
+	return true
+}
+
+func (z *Point) StringCompressed() string {
+	cstr := C.mpECP_alloc_out_str(z.ecp, C.int(1))
+	gstr := C.GoString(cstr)
+	C.free(unsafe.Pointer(cstr))
+	return gstr
+}
+
+func (z *Point) StringUncompressed() string {
+	cstr := C.mpECP_alloc_out_str(z.ecp, C.int(0))
+	gstr := C.GoString(cstr)
+	C.free(unsafe.Pointer(cstr))
+	return gstr
+}
+
+func (z *Point) Cmp(a *Point) int {
+	return int(C.mpECP_cmp(z.ecp, a.ecp))
+}
+
+func (z *Point) Add(a, b *Point) *Point {
+	C.mpECP_add(z.ecp, a.ecp, b.ecp)
+	z.cv = a.cv
+	return z
+}
+
+func (z *Point) Sub(a, b *Point) *Point {
+	C.mpECP_sub(z.ecp, a.ecp, b.ecp)
+	z.cv = a.cv
+	return z
+}
+
+func (z *Point) Neg(a *Point) *Point {
+	C.mpECP_neg(z.ecp, a.ecp)
+	z.cv = a.cv
+	return z
+}
+
+func (z *Point) Set(a *Point) *Point {
+	C.mpECP_set(z.ecp, a.ecp)
+	z.cv = a.cv
+	return z
+}
+
+func (z *Point) Mul(a *Point, b *big.Int) *Point {
+	var bmpz C.__mpz_struct
+
+	C.mpz_init(&bmpz)
+	bigint_to_c_mpz_signed(&bmpz, b)
+
+	if C._mpECP_supports_base_mul(a.ecp) != 0 {
+		C.mpECP_scalar_base_mul_mpz(z.ecp, a.ecp, &bmpz)
+	} else {
+		C.mpECP_scalar_mul_mpz(z.ecp, a.ecp, &bmpz)
+	}
+	z.cv = a.cv
+
+	return z
+}
+
+func (z *Point) SetupBaseMul() {
+	C.mpECP_scalar_base_mul_setup(z.ecp)
+	return
+}
---
+++ ecgo/point_test.go
@@ -0,0 +1,197 @@
+//BSD 3-Clause License
+//
+//Copyright (c) 2018, jadeblaquiere
+//All rights reserved.
+//
+//Redistribution and use in source and binary forms, with or without
+//modification, are permitted provided that the following conditions are met:
+//
+//* Redistributions of source code must retain the above copyright notice, this
+//  list of conditions and the following disclaimer.
+//
+//* Redistributions in binary form must reproduce the above copyright notice,
+//  this list of conditions and the following disclaimer in the documentation
+//  and/or other materials provided with the distribution.
+//
+//* Neither the name of the copyright holder nor the names of its
+//  contributors may be used to endorse or promote products derived from
+//  this software without specific prior written permission.
+//
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+//FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+//OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+//OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package ecgo
+
+import (
+	"fmt"
+	"math/big"
+	"testing"
+)
+
+func TestInitPointNeutral(t *testing.T) {
+	clist := CurveNames()
+	for _, cvn := range clist {
+		//fmt.Println("Curve found", cvn)
+		cv := NamedCurve(cvn)
+		if cv == nil {
+			fmt.Println("Error: nil returned from NamedCurve")
+			t.FailNow()
+		}
+		pt := NewPointNeutral(cv)
+		if pt == nil {
+			fmt.Println("Error: nil returned from NewPointNeutral")
+			t.FailNow()
+		}
+		if (cv.Type() == EQTypeShortWeierstrass) ||
+			(cv.Type() == EQTypeMontgomery) {
+			isNeutral := pt.IsNeutral()
+			if isNeutral != true {
+				fmt.Println("Error: neutral point is not neutral?")
+				t.FailNow()
+			}
+		}
+	}
+}
+
+func TestInitPointG(t *testing.T) {
+	clist := CurveNames()
+	for _, cvn := range clist {
+		//fmt.Println("Curve G Pt:", cvn)
+		cv := NamedCurve(cvn)
+		if cv == nil {
+			fmt.Println("Error: nil returned from NamedCurve")
+			t.FailNow()
+		}
+		pt := NewPointGenerator(cv)
+		if pt == nil {
+			fmt.Println("Error: nil returned from NewPointNeutral")
+			t.FailNow()
+		}
+		isNeutral := pt.IsNeutral()
+		if isNeutral == true {
+			fmt.Println("Error: neutral point is not neutral?")
+			t.FailNow()
+		}
+		x, y := pt.Affine()
+		if x.Cmp(cv.GetAttr("gx")) != 0 {
+			fmt.Println("Error: generator x point mismatch")
+			t.FailNow()
+		}
+		if y.Cmp(cv.GetAttr("gy")) != 0 {
+			fmt.Println("Error: generator y point mismatch")
+			t.FailNow()
+		}
+	}
+}
+
+func TestMathPointURandom(t *testing.T) {
+	clist := CurveNames()
+	for _, cvn := range clist {
+		//fmt.Println("Curve G Pt:", cvn)
+		cv := NamedCurve(cvn)
+		if cv == nil {
+			fmt.Println("Error: nil returned from NamedCurve")
+			t.FailNow()
+		}
+		npt := NewPointNeutral(cv)
+		if npt == nil {
+			fmt.Println("Error: nil returned from NewPointNeutral")
+			t.FailNow()
+		}
+		pt := NewPointURandom(cv)
+		if pt == nil {
+			fmt.Println("Error: nil returned from NewPointNeutral")
+			t.FailNow()
+		}
+		isNeutral := pt.IsNeutral()
+		if isNeutral == true {
+			fmt.Println("Error: neutral point is not neutral?")
+			t.FailNow()
+		}
+		ptstr1 := pt.StringCompressed()
+		//fmt.Println("compressed point: ", ptstr1)
+		ptcopy1 := NewPointFromString(ptstr1, cv)
+		if pt.Cmp(ptcopy1) != 0 {
+			fmt.Println("Error: export/import compressed string failed")
+			t.FailNow()
+		}
+		ptstr2 := pt.StringUncompressed()
+		ptcopy2 := NewPointFromString(ptstr2, cv)
+		if pt.Cmp(ptcopy2) != 0 {
+			fmt.Println("Error: export/import compressed string failed")
+			t.FailNow()
+		}
+		x, y := pt.Affine()
+		if x.Cmp(cv.GetAttr("gx")) == 0 {
+			fmt.Println("Error: random point matches generator X?")
+			t.FailNow()
+		}
+		if y.Cmp(cv.GetAttr("gy")) == 0 {
+			fmt.Println("Error: random point matches generator Y?")
+			t.FailNow()
+		}
+		ptcopy3 := NewPointXY(x, y, cv)
+		if pt.Cmp(ptcopy3) != 0 {
+			fmt.Println("Error: copy via X, Y failed")
+			t.FailNow()
+		}
+		pplusn := NewPointNeutral(cv)
+		pplusn.Add(pplusn, pt)
+		if pt.Cmp(pplusn) != 0 {
+			fmt.Println("Error: pt + neutral/0 != pt")
+			t.FailNow()
+		}
+		pneg := NewPointNeutral(cv).Neg(pt)
+		if pt.Cmp(pneg) == 0 {
+			fmt.Println("Error: pt == -pt")
+			t.FailNow()
+		}
+		pneg.Add(pt, pneg)
+		if pneg.Cmp(npt) != 0 {
+			fmt.Println("Error: pt + (-pt) != neutral/0")
+			t.FailNow()
+		}
+		pgen := NewPointNeutral(cv).Set(pt)
+		pgen.SetupBaseMul()
+		if pgen.Cmp(pt) != 0 {
+			fmt.Println("Error: P != Pg when Pg = Set(P)")
+			t.FailNow()
+		}
+		for i := 0; i < 10; i++ {
+			ibig := big.NewInt(int64(i))
+			pmul := NewPointNeutral(cv).Mul(pt, ibig)
+			accum := NewPointNeutral(cv)
+			for j := 0; j < i; j++ {
+				accum.Add(accum, pt)
+			}
+			if pmul.Cmp(accum) != 0 {
+				fmt.Println("Error: i * P != SUM(1 -> i, P)")
+				t.FailNow()
+			}
+			pgmul := NewPointNeutral(cv).Mul(pgen, ibig)
+			if pgmul.Cmp(pmul) != 0 {
+				fmt.Println("Error: i * P != i * Pg when Pg = P")
+				t.FailNow()
+			}
+		}
+		pdoub1 := NewPointNeutral(cv).Add(pt, pt)
+		pdoub2 := NewPointNeutral(cv).Mul(pt, big.NewInt(2))
+		if pdoub1.Cmp(pdoub2) != 0 {
+			fmt.Println("Error: P + P != 2 * P")
+			t.FailNow()
+		}
+		p2pmin1 := NewPointNeutral(cv).Sub(pdoub1, pt)
+		if p2pmin1.Cmp(pt) != 0 {
+			fmt.Println("Error: 2 * P - P != P")
+			t.FailNow()
+		}
+	}
+}
